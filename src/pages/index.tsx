723d590aaab3a15e5ff1d605b204900a437493bc
Alison G. Martins <odevalison@gmail.com>
2025-09-13 18:36:52 -0300

    feat: HeroSection component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
-import { PT_Sans_Caption } from "next/font/google";
-
-const ptSansCaption = PT_Sans_Caption({
-  weight: ["400", "700"],
-  subsets: ["latin"],
-});
+import { HeroSection } from "@/components/hero-section";
 
 export default function Home() {
   return (
-    <div className={`min-h-screen min-w-full ${ptSansCaption.className}`}></div>
+    <>
+      <article className="flex flex-col">
+        <HeroSection />
+      </article>
+    </>
   );
 }
