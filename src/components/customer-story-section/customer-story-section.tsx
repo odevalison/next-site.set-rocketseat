a68dd28a20812b83df834df8d87039d21304fd84
Alison G. Martins <odevalison@gmail.com>
2025-09-14 02:29:38 -0300

    feat: add CustomerStorySection component

---
+++ src/components/customer-story-section/customer-story-section.tsx
@@ -0,0 +1,73 @@
+import { PT_Sans_Caption } from "next/font/google";
+import Image from "next/image";
+
+const ptSansCaption = PT_Sans_Caption({
+  subsets: ["latin"],
+  weight: "700",
+});
+
+const customersStories = [
+  {
+    content:
+      "Criar minha loja com o Site.Set foi a melhor decisão para o meu negócio. A plataforma é super intuitiva, e consegui colocar meus produtos à venda em ppucos minutos.",
+    author: {
+      name: "Annette Bones",
+      role: "CEO na Anne Corp",
+      avatar: "/customer-01.png",
+    },
+  },
+  {
+    content:
+      "Transformar minha ideia em uma loja online foi fácil e rápido. Adorei as opções de personalização e a simplicidade para gerenciar os pedidos. Já vejo meus produtos alcançando mais pessoas!",
+    author: {
+      name: "Jacob Jones",
+      role: "CEO na JJ Org",
+      avatar: "/customer-02.png",
+    },
+  },
+];
+
+export const CustomerStorySection = () => {
+  return (
+    <section className="container py-8 md:py-10">
+      <div className="flex flex-col items-center gap-12">
+        <h2
+          className={`${ptSansCaption.className} text-heading-xl text-gray-100`}
+        >
+          Quem utiliza, aprova!
+        </h2>
+
+        <div className="grid gap-8 md:grid-cols-2">
+          {customersStories.map((story) => (
+            <div
+              key={story.author.name}
+              className="flex justify-center flex-col gap-6 rounded-lg bg-gray-500 p-6 md:p-10 border border-gray-400"
+            >
+              <p className="text-body-md text-gray-200">{story.content}</p>
+
+              <div className="flex items-center gap-3">
+                <div className="relative size-10 overflow-hidden rounded-full">
+                  <Image
+                    src={story.author.avatar}
+                    alt={story.author.name}
+                    fill
+                    className="object-cover"
+                  />
+                </div>
+
+                <div className="flex flex-col gap-1">
+                  <strong className="text-gray-200 text-sm">
+                    {story.author.name}
+                  </strong>
+                  <span className="text-xs text-gray-300">
+                    {story.author.role}
+                  </span>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
